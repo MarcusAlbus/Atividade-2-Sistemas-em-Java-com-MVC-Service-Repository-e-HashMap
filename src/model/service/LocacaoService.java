300737628334c7eba6fd1ff65c56fe66f2bbc2e0
camile_wengrzynek <camileweng@gmail.com>
2026-05-29 21:00:49 -0300

    implementando LocacaoService

--- src/model/service/LocacaoService.java
+++ src/model/service/LocacaoService.java
@@ -5,8 +5,43 @@ import entity.Aluno;
 
 public class LocacaoService {
 
-    public void validarMatricula(Aluno aluno){
-        if (aluno == null && aluno)
+    private LocacaoRepository locacaoRepository;
+    private AlunoRepository alunoRepository;
+    private EquipamentoRepository equipamentoRepository; //precisa das classes aqui
+
+    public LocacaoService(LocacaoRepository locacaoRepository, AlunoRepository alunoRepository, EquipamentoRepository equipamentoRepository){
+        this.locacaoRepository = locacaoRepository;
+        this.alunoRepository = alunoRepository;
+        this.equipamentoRepository = equipamentoRepository;
     }
+
+    public Locacao cadastrarLocacao(Locacao locacao){
+        if(!alunoRepository.verificarExistenciaAluno(locacao.getAluno().getId())){
+            throw new RuntimeException("O aluno não existe.");
+        }
+
+        if(!equipamentoRepository.verificarExistenciaEquipamento(locacao.getEquipamento().getId())){
+            throw new RuntimeException("O equipamento não existe.");
+        }
+
+        if(!equipamentoRepository.verificarDisponibilidadeEquipamento(locacao.getEquipamento().getId())){
+            throw new RuntimeException("O equipamento está indisponível.");
+        }
+
+        locacao.getEquipamento().setDisponivel(false);
+        return locacaoRepository.criarLocacao(locacao);
+    }
+
+    public List<Locacao> listarLocacao(){
+        return locacaoRepository.listarLocacoes();
+    }
+
+    public Locacao removerLocacao(int id){
+        locacaoRepository.disponibilizarEquipamento(id).setDisponivel(true);
+        if(locacaoRepository.devolverLocacao(id).getStatus() == false)
+            throw new RuntimeException("Locação finalizada.");
+        return locacaoRepository.removerLocacao(id);
+    }
+
 
 }
